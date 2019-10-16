b05a94c60c1ccdbaf0c2c6cd7b79529ee88eb0de
Max <sytemap@mail.ru>
2019-10-16 09:28:30 +0300

    Update WebhookApplication.java

--- src/main/java/com/webapp/webhook/WebhookApplication.java
+++ src/main/java/com/webapp/webhook/WebhookApplication.java
@@ -2,6 +2,7 @@ package com.webapp.webhook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class WebhookApplication {
