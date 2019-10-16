164e418be1b41d1523c7df1c01af7bb34d5f7ee6
Max <sytemap@mail.ru>
2019-10-16 09:37:28 +0300

    Update WebhookApplication.java

--- src/main/java/com/webapp/webhook/WebhookApplication.java
+++ src/main/java/com/webapp/webhook/WebhookApplication.java
@@ -10,12 +10,10 @@ public class WebhookApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebhookApplication.class, args);
 	}
-
-	@Service
-	public class WebhookApplication {
-    	int sum(int a, int b) {
-        	return a + b;
-     }
 }
-
+@Service
+public class Webhook {
+	int sum(int a, int b) {
+		return a + b;
+	}
 }
