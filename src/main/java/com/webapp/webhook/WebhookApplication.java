89dd67fbd07a3fb3101d3a82db286ddb0be7d831
Max <sytemap@mail.ru>
2019-10-16 09:39:39 +0300

    Update WebhookApplication.java

--- src/main/java/com/webapp/webhook/WebhookApplication.java
+++ src/main/java/com/webapp/webhook/WebhookApplication.java
@@ -2,18 +2,11 @@ package com.webapp.webhook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class WebhookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebhookApplication.class, args);
-	}
-}
-@Service
-public class Webhook {
-	int sum(int a, int b) {
-		return a + b;
 	}
 }
